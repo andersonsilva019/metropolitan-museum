9f2e87cbbf1c6420fe25137d3d18f62aba36c40c
Anderson Silva <andersonnsilva@alu.ufc.br>
2023-06-18 20:20:58 -0300

    creating the ArtList component

---
+++ src/components/ArtList/index.tsx
@@ -0,0 +1,19 @@
+import { ArtCard } from '../ArtCard'
+import { Container } from './styles'
+
+export function ArtList(){
+  return(
+    <Container>
+      <ArtCard />
+      <ArtCard />
+      <ArtCard />
+      <ArtCard />
+      <ArtCard />
+      <ArtCard />
+      <ArtCard />
+      <ArtCard />
+      <ArtCard />
+      <ArtCard />
+    </Container>
+  )
+}
