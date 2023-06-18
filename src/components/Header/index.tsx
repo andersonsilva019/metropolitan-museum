d35f4c21662c88d43c8b82d91999619f2fc565ae
Anderson Silva <andersonnsilva@alu.ufc.br>
2023-06-18 19:41:15 -0300

    creating the Header component

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,11 @@
+import { SearchInput } from "./SearchInput";
+
+import { Container } from './styles'
+
+export function Header(){
+  return(
+    <Container>
+      <SearchInput />
+    </Container>
+  )
+}
