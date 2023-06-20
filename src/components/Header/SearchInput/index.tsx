062fc857ef3a2a4b7193d355d252b652a5276121
Anderson Silva <andersonnsilva@alu.ufc.br>
2023-06-20 16:36:31 -0300

    update SearchInput component

--- src/components/Header/SearchInput/index.tsx
+++ src/components/Header/SearchInput/index.tsx
@@ -1,15 +1,40 @@
+import { useNavigate } from "react-router-dom";
 import { SearchIcon } from "../../../assets/icons";
 import { SearchContainer, Input, Button } from './styles'
 
-export function SearchInput() {
+type SearchInputProps = {
+  handleSearch: () => void;
+  onSearch: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  value: string;
+}
+
+export function SearchInput({
+  onSearch,
+  value,
+  handleSearch
+}: SearchInputProps) {
+
+  const navigate = useNavigate()
+
+  function handleSubmit(event: React.FormEvent<HTMLFormElement>){
+    event.preventDefault()
+    handleSearch()
+
+    if(window.location.pathname === '/favorites') {
+      navigate('/')
+    }
+  }
+
   return (
-    <SearchContainer>
-      <Button>
-        <SearchIcon color="#B8B9BE"/>
+    <SearchContainer onSubmit={handleSubmit}>
+      <Button type="submit">
+        <SearchIcon color="#B8B9BE" />
       </Button>
       <Input
         type="text"
         placeholder="Search for a art"
+        onChange={onSearch}
+        value={value}
       />
     </SearchContainer>
   )
--- src/components/Header/SearchInput/styles.ts
+++ src/components/Header/SearchInput/styles.ts
@@ -1,6 +1,6 @@
 import { styled } from '../../../styles'
 
-export const SearchContainer = styled('div', {
+export const SearchContainer = styled('form', {
   display: 'flex',
   alignItems: 'center',
   gap: '0.5rem',
@@ -10,13 +10,20 @@ export const SearchContainer = styled('div', {
   width: '100%',
   maxWidth: '40rem',
   border: '1px solid $white300',
-  boxShadow: '0px 0px 3px rgba(0, 0, 0, 0.1)'
+  boxShadow: '0px 0px 3px rgba(0, 0, 0, 0.1)',
+
+  $$shadowColor: '$colors$secondary',
+
+  '&:focus-within': {
+    boxShadow: '0 0 0 0.1rem $$shadowColor'
+  }
 })
 
 export const Button = styled('button', {
   background: 'transparent',
   border: 0,
-  cursor: 'pointer'
+  cursor: 'pointer',
+  outline: 'none',
 })
 
 export const Input = styled('input', {
@@ -27,7 +34,7 @@ export const Input = styled('input', {
   border: 0,
   outline: 'none',
   backgroundColor: 'transparent',
-  
+
   '&::placeholder': {
     fontWeight: '400',
     color: 'rgba(0, 0, 0, 0.25)'
