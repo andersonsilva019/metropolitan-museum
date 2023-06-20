ae2931971b0802c65907a951e4cfa8d4a1c0fff5
Anderson Silva <andersonnsilva@alu.ufc.br>
2023-06-20 16:39:26 -0300

    update App

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,30 @@
+import { QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { globalStyles } from './styles/globalStyles'
-import { Home } from './pages/Home'
+import { queryClient } from './lib/react-query'
+import { DepartmentProvider } from './context/Department'
+import { SearchProvider } from './context/Search'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { routes } from './routes'
+import { FavoritesProvider } from './context/Favorites'
 
 globalStyles()
 
+const router = createBrowserRouter(routes);
+
 function App() {
-  return <Home />
+  return (
+    <QueryClientProvider client={queryClient}>
+      <FavoritesProvider>
+        <SearchProvider>
+          <DepartmentProvider>
+            <RouterProvider router={router} />
+          </DepartmentProvider>
+        </SearchProvider>
+      </FavoritesProvider>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  )
 }
 
 export default App
