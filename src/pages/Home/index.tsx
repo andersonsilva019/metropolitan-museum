b9d7157c2d77c301e5b5a60c7c28f12cdec68fc7
Anderson Silva <andersonnsilva@alu.ufc.br>
2023-06-20 16:33:30 -0300

    creating Home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,16 +1,36 @@
+import { useContext } from 'react'
 import { ArtList } from '../../components/ArtList'
-import { Filter } from '../../components/Filter'
-import { Header } from '../../components/Header'
-import { Sidebar } from '../../components/Sidebar'
-import { Container } from './styles'
+import { DefaultLayout } from '../../components/Layout/Default'
+import { DepartmentContext } from '../../context/Department'
+import { SearchContext } from '../../context/Search'
+import { useGetObjectByDepartment, useGetObjects, useSearchObject } from '../../services/MuseumApiService'
 
-export function Home() {
+export function HomePage() {
+
+  const { departmentId } = useContext(DepartmentContext)
+  const { query } = useContext(SearchContext)
+
+  const { data: artsListByDepartment, isFetching: loadingObjectByDepartment } = useGetObjectByDepartment(departmentId)
+  const { data: artsList, isFetching: loadingObjects } = useGetObjects()
+  const { data: artListBySearch, isFetching: loadingObjectBySearch } = useSearchObject(query)
+
+  if (loadingObjects || loadingObjectByDepartment || loadingObjectBySearch) {
+    return (
+      <DefaultLayout>
+        <h2>Loading...</h2>
+      </DefaultLayout>
+    )
+  }
+
   return (
-    <Container>
-      <Header />
-      <Filter />
-      <Sidebar />
-      <ArtList />
-    </Container>
+    <DefaultLayout>
+      {!query && artsListByDepartment?.length !== 0 && <ArtList data={artsListByDepartment || []} />}
+      {!query && !artsListByDepartment && <ArtList data={artsList || []} />}
+      {query && (artListBySearch || []).length > 0 ? <ArtList data={artListBySearch || []} /> : !artsList?.length && !artsListByDepartment?.length && (
+        <div style={{ width: '400px' }}>
+          <h2>Ups!... no results found</h2>
+        </div>
+      )}
+    </DefaultLayout>
   )
 }
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,11 +1,12 @@
 import { styled } from "../../styles";
 
-export const Container = styled('main', {
-  gridTemplateAreas: `
-    "header header"
-    "filter filter"
-    "sidebar content"
-  `,
+export const Container = styled('div', {
+  display: 'grid',
+  gridAreas: 'content',
+  padding: '8rem 8rem 2rem 16rem',
+  width: '100%',
+  marginLeft: '10rem',
   height: '100%',
-  paddingBottom: '40rem',
+  gridTemplateColumns: '1fr 1fr 1fr',
+  gap: '2rem',
 })
